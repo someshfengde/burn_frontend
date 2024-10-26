35a8467120db19202037c8b8e5087b3570333087
someshfengde <someshfengde@gmail.com>
2024-10-26 10:20:33 +0530

    Refactor Navbar component and remove unused code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import NavLink from './NavLink';
-import Image from 'next/image';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 const Navbar = () => {
